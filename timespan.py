6eb29feee430044859618b41f52e7b3fd8cac0fb
Rahul Pon <theflyingrahul@gmail.com>
2020-05-05 02:03:24 +0530

    Data from table has been parsed as JSON

--- timespan.py
+++ timespan.py
@@ -30,15 +30,18 @@ merge_format = workbook.add_format({
     'valign': 'vleft',
     'fg_color': '#87CEEB'})
 
-
 # subject_list = ['x', 'y', 'z', 'w', 't']
 # department = ""
 # START_DATE = ""
 # END_DATE = ""
 
+stud_att_dict = dict()
+stud_name_dict = dict()
+sub_code_name = []
+
 
 def mysql_connection():
-    global department
+    global department, stud_att_dict, sub_code_name
 
     config = {
         'user': 'root',
@@ -51,15 +54,28 @@ def mysql_connection():
     try:
         cnx = mysql.connector.connect(**config)
 
+        user_cursor = cnx.cursor()
+        query = "SELECT reg_no, name FROM xstack.users WHERE dept = %s AND year = %s"
+        user_cursor.execute(query, (department, 1))
+
+        for row in user_cursor:
+            stud_name_dict[row[0]] = row[1]
+        user_cursor.close()
+
         cursor = cnx.cursor()
 
         query = "SELECT * FROM xstack.time_table WHERE dept = %s AND sem = %s"
         cursor.execute(query, (department, 1))
 
         sub_pk = []
+        sub_code_name = []
         for row in cursor:
-            print(row[8])
-            sub_pk.append(row[8])
+            if row[8] not in sub_pk:
+                sub_pk.append(row[8])
+            t = (row[2], row[3])
+            if t not in sub_code_name:
+                sub_code_name.append(t)
+        print(sub_pk, sub_code_name)
 
         cursor.close()
 
@@ -67,19 +83,52 @@ def mysql_connection():
             looped_cursor = cnx.cursor()
             looped_query = "SELECT * FROM xstack." + str.lower(row)
             looped_cursor.execute(looped_query)
-            print(looped_cursor.column_names)
 
             date_times = looped_cursor.column_names[2:]
             required_dates = []
+            index_dates = []
 
             for date in date_times:
                 comparand = datetime.strptime(date.split(" ")[0], "%Y-%m-%d").strftime("%Y-%m-%d")
                 if START_DATE <= comparand <= END_DATE:
                     required_dates.append(date)
-            print(required_dates)
+                    index_dates.append(date_times.index(date))
 
-            looped_cursor.fetchall()
+            mod_rows = []
+            for row in looped_cursor:
+                eval_row = []
+                for index in index_dates:
+                    eval_row.append(row[index + 2])
+                pres = eval_row.count(1)
+                abs = eval_row.count(0)
+                od = eval_row.count(2)
+                total_days = len(eval_row)
+                att_percent = (pres + od) / total_days * 100
+
+                insert_instance = (total_days, pres, od, abs, att_percent)
+
+                if row[0] in stud_att_dict:
+                    stud_att_dict[row[0]].append(insert_instance)
+                else:
+                    stud_att_dict[row[0]] = [insert_instance]
+                mod_rows.append(row[0])
+
+            na_list = []
+            for reg in stud_name_dict:
+                if reg not in mod_rows:
+                    na_list.append(reg)
+
+            null_tuple = ('NA', 'NA', 'NA', 'NA', 'NA')
+            for reg in na_list:
+                if reg in stud_att_dict:
+                    stud_att_dict[reg].append(null_tuple)
+                else:
+                    stud_att_dict[reg] = [null_tuple]
+
             looped_cursor.close()
+
+        # print(stud_name_dict)
+        print(stud_att_dict)
 
         cnx.close()
 
@@ -109,9 +158,26 @@ def prepare_workbook():
 
     worksheet.merge_range('A1:Z1', 'xStack: Exported Data - Loyola-ICAM College of Engineering and Technology',
                           merge_format_head)
-    worksheet.merge_range('A2:Z2', 'Department of ' + department, merge_format_head)
-    worksheet.merge_range('A3:Z3', 'Student attendance for the period:' + START_DATE + ' to ' + END_DATE,
-                          merge_format_head)
+
+    if department == "DIT":
+        dept = "Information Technology"
+    elif department == "DCSE":
+        dept = "Computer Science and Engineering"
+    elif department == "DEEE":
+        dept = "Electrical and Electronics Engineering"
+    elif department == "DECE":
+        dept = "Electronics and Communication Engineering"
+    elif department == "DMEA":
+        dept = "Mechanical Engineering - A"
+    elif department == "DMEB":
+        dept = "Mechanical Engineering - B"
+    else:
+        dept = "Unknown"
+
+    worksheet.merge_range('A2:Z2', 'Department of ' + dept, merge_format_head)
+
+    worksheet.merge_range('A3:Z3', 'Student attendance for the period:' + str(START_DATE).split(" ")[0] + ' to ' +
+                          str(END_DATE).split(" ")[0], merge_format_head)
     worksheet.merge_range('A4:A5', 'Registration #', merge_format)
     worksheet.merge_range('B4:B5', 'Name of the student', merge_format)
 
@@ -119,9 +185,9 @@ def prepare_workbook():
     for subject in subject_list:
         prepare_subject_columns(alpha, subject)
         alpha = chr(ord(alpha) + 4)
-        print(alpha)
 
     workbook.close()
+
 
 department = sys.argv[1]
 subject_list = ast.literal_eval(sys.argv[2])
@@ -130,5 +196,4 @@ END_DATE = datetime.strptime(sys.argv[4], "%Y-%m-%d").strftime("%Y-%m-%d")
 
 mysql_connection()
 
-print(department, subject_list, START_DATE, END_DATE)
 prepare_workbook()
