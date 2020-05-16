d00b513c1083ff8786aa27bc8f2c53c5ad811d65
Rahul Pon <theflyingrahul@gmail.com>
2020-05-16 23:21:50 +0530

    modify_rest_ds_API.py is complete, returns JSON for selected hour.

---
+++ modify_rest_ds_API.py
@@ -0,0 +1,74 @@
+import sys
+from datetime import time
+
+import mysql.connector
+from mysql.connector import errorcode
+
+
+def mysql_connection():
+    global username, auth_token, day, hour
+    config = {
+        'user': 'root',
+        'password': 'vcvra-1002',
+        'host': '127.0.0.1',
+        'database': 'xstack',
+        'raise_on_warnings': True
+    }
+
+    try:
+        cnx = mysql.connector.connect(**config)
+
+        cursor = cnx.cursor()
+        query = "SELECT * FROM xstack.time_table WHERE staff_email = %s AND (weekday = %s AND hour = %s)"
+        cursor.execute(query, (username, day, hour))
+
+        response = "invalid-request"
+        for row in cursor:
+            year = "NA"
+            if row[5] == 1 or row[5] == 2:
+                year = "I"
+            if row[5] == 3 or row[5] == 4:
+                year = "II"
+            if row[5] == 5 or row[5] == 6:
+                year = "III"
+            if row[5] == 7 or row[5] == 8:
+                year = "IV"
+            response = {"department": row[4], "year": year, "semester": str(row[5]), "subject": row[2] + " - " + row[3]}
+        print(response)
+
+        cnx.close()
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password!")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist!")
+        else:
+            print(err)
+
+
+def find_hour(param):
+    if time(8, 0, 0) <= param < time(8, 55, 0):
+        return 1
+    elif time(8, 55, 0) <= param < time(9, 50, 0):
+        return 2
+    elif time(10, 10, 0) <= param < time(11, 0, 0):
+        return 3
+    elif time(11, 0, 0) <= param < time(11, 50, 0):
+        return 4
+    elif time(11, 50, 0) <= param < time(12, 40, 0):
+        return 5
+    elif time(1, 30, 0) <= param < time(2, 20, 0):
+        return 6
+    elif time(2, 20, 0) <= param < time(3, 10, 0):
+        return 7
+    elif time(3, 10, 0) <= param < time(4, 10, 0):
+        return 8
+
+
+username = sys.argv[1]
+auth_token = sys.argv[2]
+hour = sys.argv[3]
+# day = datetime.today().strftime('%A')
+day = 'Monday'
+mysql_connection()
