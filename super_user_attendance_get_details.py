6a17525982055c751be00408387cd8c5ce0cee60
Rahul Pon <theflyingrahul@gmail.com>
2020-05-20 22:44:49 +0530

    super_user_attendance_get_details.py complete, yet to run tests.

--- super_user_attendance_get_details.py
+++ super_user_attendance_get_details.py
@@ -1,6 +1,6 @@
 #!C:/Users/danie/AppData/Local/Programs/Python/Python38-32/python.exe
 import sys
-from datetime import time
+from datetime import time, datetime
 
 import mysql.connector
 from mysql.connector import errorcode
@@ -24,21 +24,123 @@ def mysql_connection():
             department=department, semester=semester, date=str(date), hour=hour)
         cursor.execute(query)
 
-        # block if super time table is true
-        # todo
-        cursor.close()
-        # return
+        # run block if super time table is true
 
-        # check in super time table
+        for row in cursor:
+            year = "NA"
+            if row[5] == 1 or row[5] == 2:
+                year = "I"
+            if row[5] == 3 or row[5] == 4:
+                year = "II"
+            if row[5] == 5 or row[5] == 6:
+                year = "III"
+            if row[5] == 7 or row[5] == 8:
+                year = "IV"
+            response = {"department": row[4], "year": year, "semester": str(row[5]), "subject_code": row[3],
+                        "subject_name": row[2], "subCode_dept_sem": row[8].lower()}
+
+            pk_table = str(row[8]).lower()
+            cursor.close()
+            cursor = cnx.cursor()
+
+            query = "SHOW COLUMNS FROM attendance." + pk_table
+            cursor.execute(query)
+
+            col_schema = []
+            for col in cursor:
+                col_schema.append(col)
+
+            col_schema = col_schema[1:]
+
+            datetimes = []
+            for col in col_schema:
+                if datetime.strptime(col[0], "%Y-%m-%d %H:%M:%S").date() == date:
+                    datetimes.append(
+                        datetime.strptime(col[0], "%Y-%m-%d %H:%M:%S")
+                    )
+
+            already_taken = False
+            for item in datetimes:
+                if str(hour) == str(find_hour(item.time())):
+                    already_taken = True
+                    required_timestamp = item
+                    break
+
+            if already_taken:
+                response['required_timestamp'] = str(required_timestamp)
+                response['datetime'] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                response['displaydate'] = str(datetime.now().strftime("%d.%m.%Y - %A"))
+                print(str(response).replace("'", '"'))
+                return
+            else:
+                response['required_timestamp'] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                response['datetime'] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                response['displaydate'] = str(datetime.now().strftime("%d.%m.%Y - %A"))
+                print(str(response).replace("'", '"'))
+                return
+        cursor.close()
+
+        # check in regular time table
         cursor = cnx.cursor()
         query = "SELECT * FROM attendance.time_table WHERE department = {department} AND semester = {semester} AND week_day = {day} AND hour = {hour}".format(
             department=department, semester=semester, day=str(day), hour=hour)
         cursor.execute(query)
 
-        # block if super time table is true
-        # todo
+        for row in cursor:
+            year = "NA"
+            if row[5] == 1 or row[5] == 2:
+                year = "I"
+            if row[5] == 3 or row[5] == 4:
+                year = "II"
+            if row[5] == 5 or row[5] == 6:
+                year = "III"
+            if row[5] == 7 or row[5] == 8:
+                year = "IV"
+            response = {"department": row[4], "year": year, "semester": str(row[5]), "subject_code": row[3],
+                        "subject_name": row[2], "subCode_dept_sem": row[8].lower()}
+
+            pk_table = str(row[8]).lower()
+            cursor.close()
+            cursor = cnx.cursor()
+
+            query = "SHOW COLUMNS FROM attendance." + pk_table
+            cursor.execute(query)
+
+            col_schema = []
+            for col in cursor:
+                col_schema.append(col)
+
+            col_schema = col_schema[1:]
+
+            datetimes = []
+            for col in col_schema:
+                if datetime.strptime(col[0], "%Y-%m-%d %H:%M:%S").date() == date:
+                    datetimes.append(
+                        datetime.strptime(col[0], "%Y-%m-%d %H:%M:%S")
+                    )
+
+            already_taken = False
+            for item in datetimes:
+                if str(hour) == str(find_hour(item.time())):
+                    already_taken = True
+                    required_timestamp = item
+                    break
+
+            if already_taken:
+                response['required_timestamp'] = str(required_timestamp)
+                response['datetime'] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                response['displaydate'] = str(datetime.now().strftime("%d.%m.%Y - %A"))
+                print(str(response).replace("'", '"'))
+                return
+            else:
+                response['required_timestamp'] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                response['datetime'] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                response['displaydate'] = str(datetime.now().strftime("%d.%m.%Y - %A"))
+                print(str(response).replace("'", '"'))
+                return
+
         cursor.close()
-        # return
+
         cnx.close()
 
     except mysql.connector.Error as err:
